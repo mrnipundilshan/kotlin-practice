1d391193c172c6e58e41c515622dafb6cb204f32
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-06-17 08:11:55 +0530

    remove unused imports

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.myapplication
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.net.toUri
 
 class MainActivity : AppCompatActivity() {
