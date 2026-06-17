502df25411dcbade0c0c44b5e8327e8736c59713
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-06-16 20:43:15 -0500

    explicit and implicit learn

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.net.toUri
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +17,24 @@ class MainActivity : AppCompatActivity() {
         Thread.sleep(3000)
         installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        //Explicit Intent
+        val explicitButton = findViewById<Button>(R.id.explicitButton)
+
+        explicitButton.setOnClickListener {
+            val explicitIntent = Intent(this, secondActivity::class.java)
+            startActivity(explicitIntent)
+            //finish()
+        }
+
+        // Implicit Intent
+        val url = "https://www.google.com"
+        val implicitButton = findViewById<Button>(R.id.implicitButton)
+
+        implicitButton.setOnClickListener { 
+            val implicitIntent = Intent(Intent.ACTION_VIEW, url.toUri())
+            startActivity(implicitIntent)
+        }
 
     }
 }
