dbf1e49b3e52cf2cccfc417ae9c9b98e199b7bd1
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-06-17 09:18:02 +0530

    list view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,34 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Thread.sleep(3000)
         installSplashScreen()
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView: ListView = findViewById(R.id.listView)
+        val listItems = arrayOf(
+            "Read a Book",
+            "Play Cricket",
+            "Learn Kotlin",
+            "Go For Shopping",
+            "Attend a Seminar"
+        )
+
+        val listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        listView.adapter = listAdapter
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position) as String
+            Toast.makeText(this, "You Have Clicked on: $selectedItem", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
