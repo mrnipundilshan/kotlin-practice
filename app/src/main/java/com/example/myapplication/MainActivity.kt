dc175c944b2a3aca7e21697c987c662db14cccbc
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-06-18 01:52:53 -0500

    spinner background remove

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -8,11 +13,34 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var spinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(3000)
         installSplashScreen()
         setContentView(R.layout.activity_main)
 
+        spinner = findViewById(R.id.spinner)
+
+        val listItems = listOf("Chocolate 🍫", "Vanilla 🍦", "Strawberry 🍓", "Mango 🥭")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = arrayAdapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>, view : View?, position: Int, id: Long){
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                Toast.makeText(this@MainActivity, "You have selected $selectedItem flavor", Toast.LENGTH_SHORT).show()
+            }
+            override fun onNothingSelected(p0: AdapterView<*>){
+                
+            }
+        }
+
     }
+
 }
