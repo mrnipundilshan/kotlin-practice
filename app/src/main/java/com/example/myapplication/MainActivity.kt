bc71d8df0b2bc1a23f5354c3a47b7ce51285fda2
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-06-18 06:48:03 -0500

    Fragment

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,40 @@
 package com.example.myapplication
 
+import android.R.attr.fragment
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(3000)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonFragment1.setOnClickListener {
+            goToFragment(Fragment1())
+        }
+
+        binding.buttonFragment2.setOnClickListener {
+            goToFragment(Fragment2())
+        }
+    }
+
+    private fun goToFragment(fragment: Fragment){
+        fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
     }
 }
