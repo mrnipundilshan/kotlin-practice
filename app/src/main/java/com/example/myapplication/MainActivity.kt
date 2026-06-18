a1235bb5d04defce8a215aff7e6d2b458b65f844
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-06-18 05:42:28 -0500

    alert box

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
@@ -14,5 +17,22 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
         setContentView(R.layout.activity_main)
 
+        val showButton: Button = findViewById(R.id.showButton)
+
+        showButton.setOnClickListener { showAlertDialog() }
+    }
+
+    private fun showAlertDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Snapchat")
+            .setMessage("Do you want to uninstall the app?")
+            .setPositiveButton("Yes"){dialog, which ->
+                Toast.makeText(this, "The app is successfully uninstalled", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("No"){dialog, which ->
+                dialog.dismiss()
+            }
+        val alertDialog : AlertDialog = builder.create()
+        alertDialog.show()
     }
 }
