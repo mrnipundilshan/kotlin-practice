652617b77fa9a5acf0e653fe14a2d6e786256285
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-06-19 10:29:45 +0530

    viewPager2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,76 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+
+    // step 1: Declare Variables
+    private lateinit var viewPager: ViewPager2
+    private lateinit var pagerAdapter: ViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(3000)
         installSplashScreen()
         setContentView(R.layout.activity_main)
 
+        // step 6 : set Adapter
+        viewPager = findViewById(R.id.viewPager)
+        pagerAdapter = ViewPagerAdapter()
+        viewPager.adapter = pagerAdapter
+    }
+}
+
+// step 2: create viewPagerAdapter Class
+class ViewPagerAdapter: RecyclerView.Adapter<ViewHolder>(){
+
+    // step 4 : Create a List
+    private val itemList = listOf(
+        "Item 1",
+        "Item 2",
+        "Item 3",
+        "Item 4",
+        "Item 5"
+    )
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.page_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
+        val item = itemList[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+}
+
+// step 3: create ViewHolder Class
+class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    private val pagerText: TextView = itemView.findViewById(R.id.pagerText)
+    fun bind(item: String){
+        pagerText.text = item
     }
 }
