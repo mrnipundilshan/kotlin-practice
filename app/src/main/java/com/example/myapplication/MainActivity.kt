e938516d33fd6ef4193d992ec7b0cd8ddd5b7821
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-06-22 11:36:10 +0530

    fix issues

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,45 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import com.example.myapplication.databinding.ActivityMainBinding
+import kotlin.math.pow
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(3000)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.calculateBtn.setOnClickListener {
+            calculateBMI()
+        }
     }
+
+    private fun calculateBMI(){
+        val weight = binding.weightEdit.text.toString().toFloatOrNull()
+        val height = binding.heightEdit.text.toString().toFloatOrNull()
+
+        if (weight != null && height != null){
+            val bmi = weight / (height/100).pow(2)
+            val bmiResult = String.format("%.2f", bmi)
+
+            val bmiCategory = when {
+                bmi < 18.5 -> "Under Weight"
+                bmi < 25 -> "Normal Weight"
+                bmi < 30 -> "Over Weight"
+                else -> "Obese"
+            }
+            binding.resultText.text = "BMI: $bmiResult\nCategory: $bmiCategory"
+        } else {
+            binding.resultText.text = "Invalid Input"
+        }
+    }
+
+
 }
