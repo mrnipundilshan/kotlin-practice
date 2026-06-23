aa6f382ff5a2246101fde78e8cdaf28a5f7266fc
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-06-23 11:52:15 +0530

    Quiz app

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,103 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val questions = arrayOf("What is the built-in database in Android Studio?", "What is the full form of APK in Android Development?", "In which year, first android was released by Google")
+
+    private val options = arrayOf(arrayOf("MySQL", "SQLite", "Firebase"), arrayOf("Application Programming Interface", "Android Programming Interface", "Android Package Information"),
+        arrayOf("2010", "2006" , "2008"))
+
+    private val correctAnswers = arrayOf(1, 0, 2)
+
+    private var currentQuestionIndex = 0
+    private var score = 0
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(3000)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+        displayQuestion()
+        binding.option1Button.setOnClickListener { checkAnswer(0) }
+        binding.option2Button.setOnClickListener { checkAnswer(1) }
+        binding.option3Button.setOnClickListener { checkAnswer(2) }
+
+        binding.restartButton.setOnClickListener { restartQuiz() }
+    }
+
+    private fun correctButtonColors(buttonIndex: Int){
+        when(buttonIndex){
+            0 -> binding.option1Button.setBackgroundColor(Color.GREEN)
+            1 -> binding.option2Button.setBackgroundColor(Color.GREEN)
+            2 -> binding.option3Button.setBackgroundColor(Color.GREEN)
+        }
+    }
+
+    private fun wrongButtonColors(buttonIndex: Int){
+        when(buttonIndex){
+            0 -> binding.option1Button.setBackgroundColor(Color.RED)
+            1 -> binding.option2Button.setBackgroundColor(Color.RED)
+            2 -> binding.option3Button.setBackgroundColor(Color.RED)
+        }
+    }
+
+    private fun resetButtonColors(){
+        binding.option1Button.setBackgroundColor(Color.rgb(50,59,96))
+        binding.option2Button.setBackgroundColor(Color.rgb(50,59,96))
+        binding.option3Button.setBackgroundColor(Color.rgb(50,59,96))
+    }
+
+    private fun showResults(){
+        Toast.makeText(this, "Your score: $score out of ${questions.size}", Toast.LENGTH_LONG).show()
+        binding.restartButton.isEnabled = true
+    }
+
+    private  fun displayQuestion(){
+        binding.questionText.text = questions[currentQuestionIndex]
+        binding.option1Button.text = options[currentQuestionIndex][0]
+        binding.option2Button.text = options[currentQuestionIndex][1]
+        binding.option3Button.text = options[currentQuestionIndex][2]
+        resetButtonColors()
+    }
+
+    private fun checkAnswer(selectedAnswerIndex: Int){
+        val correctAnswerIndex = correctAnswers[currentQuestionIndex]
+
+        if (selectedAnswerIndex == correctAnswerIndex){
+            score++
+            correctButtonColors(selectedAnswerIndex)
+        }  else {
+            wrongButtonColors(selectedAnswerIndex)
+            correctButtonColors(correctAnswerIndex)
+        }
+        if (currentQuestionIndex < questions.size -1){
+            currentQuestionIndex++
+            binding.questionText.postDelayed({displayQuestion()}, 1000)
+        } else {
+            showResults()
+        }
+    }
+
+    private fun restartQuiz(){
+        currentQuestionIndex = 0
+        score = 0
+        displayQuestion()
+        binding.restartButton.isEnabled = false
     }
 }
