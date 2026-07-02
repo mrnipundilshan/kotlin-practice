770924c2f6d098351cb2dd46a35a1a278fc57a8d
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-07-02 11:45:41 +0530

    jetpack compose

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,69 @@
-package com.example.myapplication
+  package com.example.myapplication
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(3000)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
-
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) { Greeting() }
+            }
+        }
     }
 }
+
+@Composable
+fun Greeting() {
+    Row() {
+        Text(
+            text = "Hello World"
+        )
+        Text(
+            text = "Hello World"
+        )
+        Text(
+            text = "Hello World"
+        )
+
+    }
+
+    Column() {
+        Text(
+            text = "Hello World",
+            style = TextStyle(
+                color = Color.Red,
+               fontSize = 20.sp
+            )
+        )
+        Text(
+            text = "Hello World"
+        )
+        Text(
+            text = "Hello World"
+        )
+    }
+
+}
+
