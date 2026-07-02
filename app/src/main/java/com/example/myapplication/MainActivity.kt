06b3fdf5da4fcd8763bcf9dceb4a7674d9e0aeb4
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-07-02 11:43:31 +0530

    button-n-image

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,72 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(3000)
-        installSplashScreen()
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LearnButton()
+                }
 
+            }
+        }
     }
 }
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LearnButton(){
+
+    val context = LocalContext.current.applicationContext
+
+    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(
+            onClick = {
+                Toast.makeText(context, "Login Successful", Toast.LENGTH_LONG).show()
+                      },
+            shape = RoundedCornerShape(size = 16.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Red))
+        {
+            Text(
+                text = "Login"
+            )
+        }
+
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "Image")
+    }
+}
+
+
