ca06f52ce27c7842710536b73bdf19563796cd95
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-07-02 11:53:13 +0530

    text modifiers

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,69 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(3000)
-        installSplashScreen()
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Box (contentAlignment = Alignment.Center){
 
+                        LearnTextAndModifiers()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LearnTextAndModifiers(){
+
+    val clickOnText = {
+        print("jifdf if ")
+    }
+
+    Text(text = stringResource(id = R.string.hello_world),
+        color = Color.Red,
+        fontSize = 32.sp,
+        fontStyle = FontStyle.Italic,
+        modifier = Modifier.padding(22.dp).background(Color.Black).clickable(onClick = clickOnText )
+        )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowPreview(){
+    MyApplicationTheme() {
+        LearnTextAndModifiers()
     }
 }
